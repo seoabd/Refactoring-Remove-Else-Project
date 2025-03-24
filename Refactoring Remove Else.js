01f73e565aaebd02b019567f5daace4f7cfdf5c3
Sead Abduli <abdulisead@icloud.com>
2025-03-24 18:00:13 +0100

    Update Refactoring Remove Else.js

---
+++ Refactoring Remove Else.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ *
+ * @returns {string}
+ */
+function getNumbersEquality(a, b) {
+    if (a === b) {
+      return 'a and b are equal';
+    }
+  
+    if (a < b) {
+      return 'a < b';
+    }
+  
+    if (a > b) {
+      return 'a > b';
+    }
+  }
